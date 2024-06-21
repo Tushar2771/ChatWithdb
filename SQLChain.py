63ae331d369dc25fa03704889632364e360dd1af
Tushar2771 <tusharrane2301@gmail.com>
2024-06-21 15:24:24 +0530

    db function for table schema initial commit

---
+++ SQLChain.py
@@ -0,0 +1,180 @@
+import mysql.connector
+from langchain_community.utilities import SQLDatabase
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+
+#mysql_config
+host = 'localhost',
+port = '3306'
+user = 'tushar'
+password = 'hcdc'
+database = 'classwork_db'
+
+
+#Initializing databse Connection
+def init_database(user: str, password: str, host: str, port: str, database: str):
+  db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+  return SQLDatabase.from_uri(db_uri)
+
+
+#getting mysql_schema
+def get_table_info():
+    # Establish the database connection
+    conn = mysql.connector.connect(
+        host = 'localhost',
+        port = '3306',
+        user = 'tushar',
+        password = 'hcdc',
+        database = 'classwork_db'
+    )
+    
+    cursor = conn.cursor()
+    
+    # Query to get all tables
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    
+    db_info = []
+    
+    for (table_name,) in tables:
+        cursor.execute(f"SHOW CREATE TABLE  {table_name}")
+        columns = cursor.fetchall()
+        substring = "ENGINE=InnoDB"
+        string = columns[0][1]
+        index = string.find(substring)
+        db_info.append(string[:index])
+    
+    cursor.close()
+    conn.close()
+    
+    return db_info
+
+# Define llm outside the function for reuse
+llm = Ollama(model='mistral')  
+
+def get_sql_chain(db):
+    template = """You are interacting with a user who is asking you questions about the company's database go through the schema find the appropriate table according to que and only use the table is in schema don't create table name on your own.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+            
+    prompt = ChatPromptTemplate.from_template(template)
+    
+    
+    def get_schema(_):
+        return get_table_info()
+    
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+   
+
+def get_response(user_query: str, db: SQLDatabase, chat_history: list):
+  sql_chain = get_sql_chain(db)
+
+  schema_info = get_table_info()
+  
+  template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    <SCHEMA>{schema}</SCHEMA>
+
+    Conversation History: {chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    User question: {question}
+    SQL Response: {response}"""
+  
+  prompt = ChatPromptTemplate.from_template(template)
+  
+
+  chain = (
+    RunnablePassthrough.assign(query=sql_chain).assign(
+      schema=lambda _: schema_info,
+      response=lambda vars: db.run(vars["query"]),
+    )
+    | prompt
+    | llm
+    | StrOutputParser()
+  )
+  
+  return chain.invoke({
+    "question": user_query,
+    "chat_history": chat_history,
+  })
+    
+  
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+      AIMessage(content="Hello! I'm a SQL assistant. Ask me anything about your database."),
+    ]
+
+
+st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:")
+
+st.title("Chat with MySQL")
+
+with st.sidebar:
+    st.subheader("Mysql user setting")
+
+    
+    st.text_input("Host", value="localhost", key="Host")
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="tushar", key="User")
+    st.text_input("Password", type="password", value="hcdc", key="Password")
+    st.text_input("Database", value="classwork_db", key="Database")
+    
+    if st.button("Connect"):
+        with st.spinner("Connecting to database..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"]
+            )
+            st.session_state.db = db
+            st.success("Connected to database!")
+    
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+user_query = st.chat_input("Type a message...")
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+        
+    with st.chat_message("AI"):
+        response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
+        st.markdown(response)
+        
+    st.session_state.chat_history.append(AIMessage(content=response))
